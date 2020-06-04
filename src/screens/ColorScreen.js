b4238e73526e1932924e536b7b72c368e34466b6
심기현 <kihyunsim@simgihyeon-ui-MacBookPro.local>
2020-06-04 20:14:21 +0900

    래덤 색 쌓기

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -11,17 +11,35 @@ const ColorScreen = () => {
           setColors([...colors, randomRgb()]);
         }}
       />
-      <View
+      {/* <View
         style={{
-          width: 300,
+          width: 420,
           height: 60,
           backgroundColor: randomRgb(),
         }}
-      ></View>
+      ></View> */}
+      <FlatList
+        keyExtractor={(item) => {
+          item;
+        }}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                width: 420,
+                height: 60,
+                backgroundColor: item,
+              }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
 
+//색상을 랜덤으로 조합해주는 함수!
 const randomRgb = () => {
   const red = Math.random() * 256;
   const green = Math.random() * 256;
