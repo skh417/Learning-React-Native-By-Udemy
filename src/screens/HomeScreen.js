6ef74cca7652974faabd72d867487962a5bf219f
심기현 <kihyunsim@simgihyeon-ui-MacBookPro.local>
2020-06-04 14:42:06 +0900

    add gitignore

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>HomeScreen</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text>This is Home!</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
